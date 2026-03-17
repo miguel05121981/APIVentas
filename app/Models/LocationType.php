1aaed4f392b9a708c1a96caabe75bd3db14bfb00
miguel <miguel.castaneda@novacodesoft.com>
2026-03-16 21:40:01 -0500

    Creación de los modelos tipo de dispositivo, tipo de ubicación, tipo de movimiento y tipo de mantenimiento

--- app/Models/LocationType.php
+++ app/Models/LocationType.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
-class DeviceType extends Model
+class LocationType extends Model
 {
-    protected $table = 'devices_type';
+    protected $table = 'location_type';
 
     protected $fillable = [
         'name',
@@ -14,11 +15,18 @@ class DeviceType extends Model
         'user_update',
         'active'
     ];
+
+    /**
+     * Usuario que creó el registro
+     */
     public function userCreate()
     {
         return $this->belongsTo(User::class, 'user_create');
     }
 
+    /**
+     * Usuario que actualizó el registro
+     */
     public function userUpdate()
     {
         return $this->belongsTo(User::class, 'user_update');
